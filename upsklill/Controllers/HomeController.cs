c244b3dd4e7796706908c4816e8384626b2540e6
Justin Chizer <justin.chizer@gmail.com>
2017-12-21 10:02:40 -0800

    init

--- upsklill/Controllers/HomeController.cs
+++ upsklill/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace upsklill.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page."; //test
 
             return View();
         }
