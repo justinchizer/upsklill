fbda4da099e0e56326e4d99024e69e681304d9a6
Justin Chizer <justin.chizer@gmail.com>
2017-12-21 09:07:27 -0800

    Add project files.

---
+++ upsklill/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace upsklill.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
